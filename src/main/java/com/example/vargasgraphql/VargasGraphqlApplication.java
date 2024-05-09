75a3e8155d82fcad9e7bd08932df626ee9d75edb
Cesar Vargas Bolaños <cesar.vargas@gorillalogic.com>
2024-05-09 11:24:10 -0600

    initial commit

---
+++ src/main/java/com/example/vargasgraphql/VargasGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.vargasgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VargasGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VargasGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vargasgraphql/VargasGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vargasgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VargasGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
